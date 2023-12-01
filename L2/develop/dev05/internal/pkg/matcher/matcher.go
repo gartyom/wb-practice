20d6a08b3f71fcc19be6ae212ccff5d15e95a143
gartyom <mr.gartyom@gmail.com>
2023-12-01 23:42:46 +0300

    L2 dev05 matcher implemented as chain of resp pattern

--- L2/develop/dev05/internal/pkg/matcher/matcher.go
+++ L2/develop/dev05/internal/pkg/matcher/matcher.go
@@ -6,31 +6,60 @@ import (
 )
 
 type Matcher struct {
-	IgnoreCase *bool
-	Fixed      *bool
+	pipline Pipeline
+}
+
+type Pipeline interface {
+	Process(pattern string, s string) (bool, error)
+	SetNext(p Pipeline)
 }
 
 func New(ignoreCase *bool, fixed *bool) *Matcher {
-	return &Matcher{ignoreCase, fixed}
+	var first Pipeline
+
+	first = &Regexp{}
+	if *fixed {
+		first = &Fixed{}
+	}
+
+	if *ignoreCase {
+		first = &Lower{next: first}
+	}
+
+	return &Matcher{first}
 }
 
 func (m *Matcher) Match(pattern string, s string) (bool, error) {
-	if *m.IgnoreCase {
-		pattern = strings.ToLower(pattern)
-		s = strings.ToLower(s)
-	}
-
-	if *m.Fixed {
-		return matchFixed(pattern, s)
-	}
-
-	return match(pattern, s)
+	return m.pipline.Process(pattern, s)
 }
 
-func match(pattern string, s string) (bool, error) {
+type Lower struct {
+	next Pipeline
+}
+
+func (l *Lower) Process(pattern string, s string) (bool, error) {
+	pattern = strings.ToLower(pattern)
+	s = strings.ToLower(s)
+	return l.next.Process(pattern, s)
+}
+func (l *Lower) SetNext(p Pipeline) {
+	l.next = p
+}
+
+type Fixed struct {
+}
+
+func (f *Fixed) Process(pattern string, s string) (bool, error) {
+	return pattern == s, nil
+}
+func (l *Fixed) SetNext(p Pipeline) {
+}
+
+type Regexp struct {
+}
+
+func (r *Regexp) Process(pattern string, s string) (bool, error) {
 	return regexp.MatchString(pattern, s)
 }
-
-func matchFixed(pattern string, s string) (bool, error) {
-	return pattern == s, nil
+func (l *Regexp) SetNext(p Pipeline) {
 }
